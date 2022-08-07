86dd1de8104002dd222f5e34d1c01c85c3957748
Daniel Victor <vicdaniel.ferreira@gmail.com>
2022-08-07 13:03:18 -0300

    Update tests

--- src/tests/forth_test.spec.ts
+++ src/tests/forth_test.spec.ts
@@ -1,5 +1,5 @@
 import { URL } from '../../frontend/src/Form'
 
-it('Checar presensa URL', () => {
+it('Checar alteração URL', () => {
   expect(URL.searchURL('Estou com problemas ao acessar https://engsoftmoderna.info/')).toMatch('Estou com problemas ao acessar <a href="https://engsoftmoderna.info/">https://engsoftmoderna.info/</a>')
 })
